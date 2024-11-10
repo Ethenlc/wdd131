1199833f4d4578d1c7064d240aa2a9e74456d37b
Ethenlc <cam22015@byui.edu>
2024-11-09 18:09:42 -0700

    First push

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Update the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Update the last modified date
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
