3300964bc7558ac26d0363c55b57dcd3bac6407d
Ethenlc <cam22015@byui.edu>
2024-11-09 18:24:01 -0700

    Updated scripture

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,3 @@
-// Update the current year
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-// Update the last modified date
 document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
